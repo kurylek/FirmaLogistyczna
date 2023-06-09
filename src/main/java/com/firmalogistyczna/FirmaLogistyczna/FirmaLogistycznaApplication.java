a60f17d46a9fe23c25e35ec100b81cd4db7a271c
Kamil Kuryłek <kurylek98@gmail.com>
2023-06-09 21:58:10 +0200

    Initial commit

---
+++ src/main/java/com/firmalogistyczna/FirmaLogistyczna/FirmaLogistycznaApplication.java
@@ -0,0 +1,13 @@
+package com.firmalogistyczna.FirmaLogistyczna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirmaLogistycznaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirmaLogistycznaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/firmalogistyczna/FirmaLogistyczna/FirmaLogistycznaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firmalogistyczna.FirmaLogistyczna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirmaLogistycznaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
